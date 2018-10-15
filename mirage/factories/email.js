81b07e352c502f89a0256b62429584f928b8f5c4
Weven Wang <wang220@usc.edu>
2018-10-14 23:09:04 -0700

    first commit

---
+++ mirage/factories/email.js
@@ -0,0 +1,30 @@
+import { Factory,faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  // id()
+  // {
+  //   return
+  //
+  //
+  // },
+  from()
+  {
+    return faker.internet.email();
+
+  },
+  to()
+  {
+    return faker.internet.email();
+
+  },
+  subject()
+  {
+    return faker.lorem.sentence();
+
+  },
+  message()
+  {
+    return faker.lorem.paragraphs();
+
+  }
+});
